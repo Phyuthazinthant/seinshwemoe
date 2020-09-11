92cbc6d48c0e0b2f0222b06a4e5c79b001a09126
Phyuthazinthant <phyuthazin.mg@gmail.com>
2020-09-11 13:51:50 +0630

    contact and services

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import './App.css';
 import DiscountPage from './features/products/components/DiscountPage';
 import Footer from './app/components/Footer';
 import Routes from './app/routes'
+import Contact from './features/contacts/contact'
+import Service from './pages/ServicePage'
 
 import {BrowserRouter} from 'react-router-dom'
 export default class App extends React.Component{
   render(){
     return(
       <div style={{backgroundColor:'#f4f4f4'}}>
-      <BrowserRouter>
+       <BrowserRouter>
          <Routes />
        </BrowserRouter>
+       <Contact/>
+       <Service/>
        </div>
     )
   }
